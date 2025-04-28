8290cd3717a5c1fc9e0e001b8e162646330280fb
Nazmul H. Rupok <re.enter.rupok@gmail.com>
2025-04-28 14:35:29 +0600

    Update build files

--- public/js/blocks/badge/build/index.asset.php
+++ public/js/blocks/badge/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '038c63ac323e73e300b7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '625efb5d8392562dfb4d');
