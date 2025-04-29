03d0329ee00510d9ddf1661aaf1f7969688e5fa1
Nazmul H. Rupok <re.enter.rupok@gmail.com>
2025-04-29 12:48:54 +0600

    Fixed: Sustainability SVG rendering issue

--- public/js/blocks/badge/build/index.asset.php
+++ public/js/blocks/badge/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4f5faf235b8cf52b6972');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '780363757fa016fc5180');
